aab040147b91f117e37fd9fb43371d820f58922a
Colby Russell <29888203e3db65f5@x.colbyrussell.com>
2023-03-01 21:55:34 -0600

    Change output configuration
    These changes follow from what Joe and I talked about tonight; both the
    original project by Simon Willison and the Code for Kentuckiana
    adaptation push the scraped data to a separate repo from where the
    scraper code lives--GitHub is functioning as cheap, reliable file
    storage.
    
    The scraper configuration needs to reflect where the output data should
    live, and doesn't need to point to the scraper's own repo.

--- austinenergy_scraper.py
+++ austinenergy_scraper.py
@@ -3,8 +3,8 @@ from kubra_scraper import KubraScraper
 
 class AustinEnergyScraper(KubraScraper):
     owner = "open-austin"
-    repo = "energy-outage"
-    filepath = "lgeku/outages.json"
+    repo = "power-outage-data"
+    filepath = "outages.json"
 
     instance_id = "dd9c446f-f6b8-43f9-8f80-83f5245c60a1"
     view_id = "76446308-a901-4fa3-849c-3dd569933a51"
