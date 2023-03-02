812027dc49108fbd9ca8f40c02573e5efdc85e18
Colby Russell <29888203e3db65f5@x.colbyrussell.com>
2023-03-01 21:46:29 -0600

    Fix syntax errors from last commit

--- lgeku_scraper.py
+++ lgeku_scraper.py
@@ -6,5 +6,5 @@ class LGEKUScraper(KubraScraper):
     repo = "energy-outage"
     filepath = "lgeku/outages.json"
 
-    instanceid: 'dd9c446f-f6b8-43f9-8f80-83f5245c60a1',    
-    viewid: '76446308-a901-4fa3-849c-3dd569933a51',
+    instance_id = "dd9c446f-f6b8-43f9-8f80-83f5245c60a1"
+    view_id = "76446308-a901-4fa3-849c-3dd569933a51"
