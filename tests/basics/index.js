85742f0f3599a9873fcaf072341f6c10370af5a0
Colby Russell <31534cc47b23d420@x.colbyrussell.com>
2023-03-13 10:53:56 -0500

    Add HAR capture test data

--- tests/basics/index.js
+++ tests/basics/index.js
@@ -1,6 +1,6 @@
 import { test, read } from "$TEST_HELPER";
 
-import { AustinEnergyScraper } from "$PROJECT/AustinEnergyScraper.src";
+// XXX import { AustinEnergyScraper } from "$PROJECT/AustinEnergyScraper.src";
 import { HARMockServer } from "$PROJECT/tests/HARMockServer.src";
 
 const HAR_PATH = "$PROJECT/tests/basics/capture.har";
@@ -8,7 +8,14 @@ const HAR_PATH = "$PROJECT/tests/basics/capture.har";
 test(() => {
   let mockSystem = new HARMockServer(HAR_PATH, read);
 
-  return mockSystem.read("derp/foo/bar").then((res) => {
-    console.log(">>>>>res", res); // XXX
+  const url = ( // XXX
+    "https://maps.googleapis.com/maps/api/mapsjs/gen_204?csp_test=true"
+  );
+
+  return mockSystem.read(url).then((res) => {
+    if (res.status == 200) {
+      return Promise.resolve();
+    }
+    return Promise.reject();
   });
 })
